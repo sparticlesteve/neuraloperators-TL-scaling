ff63177248ae4e68f24860fc3e97cb779cf2e784
Steve Farrell <sparticlesteve@gmail.com>
2025-03-10 21:47:52 -0700

    debug

--- utils/misc_utils.py
+++ utils/misc_utils.py
@@ -121,7 +121,9 @@ def compute_grad_norm(p_list):
     grad_norm = 0
     print('compute_grad_norm, looping over parameters')
     for p in p_list:
-        print(p.sum(), p.grad)
+        print(p.sum())
+        if p.grad is None:
+            print(p)
         param_g_norm = p.grad.detach().data.norm(2)
         grad_norm += param_g_norm.item()**2
     grad_norm = grad_norm**0.5
